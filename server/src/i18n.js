5663612f55f32ae0612356ba3f6b739d9477d3fa
vnair2205 <vnair2205@gmail.com>
2025-09-21 17:12:07 +0530

    dfsf

---
+++ server/src/i18n.js
@@ -0,0 +1,29 @@
+// client/src/i18n.js
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import Backend from 'i18next-http-backend';
+
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json',
+    },
+    supportedLngs: ['en', 'ar', 'bn', 'gu', 'hi', 'kn', 'ml', 'mr', 'ta', 'te', 'ur'],
+    nonExplicitSupportedLngs: true,
+    load: 'languageOnly',
+  });
+
+export default i18n;
