debf78ab8bfed48fb449640bf1f3b20012d21b57
vnair2205 <vnair2205@gmail.com>
2025-09-22 15:41:25 +0530

    asdasd

---
+++ admin-panel/src/pages/UserManagementPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserManagementPage = () => {
+  return (
+    <div>
+      <h1>User Management</h1>
+      <p>This is where you will manage application users.</p>
+    </div>
+  );
+};
+
+export default UserManagementPage;
