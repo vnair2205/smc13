40453a225726b80100f0f7497e42c9cb650a3731
vnair2205 <vnair2205@gmail.com>
2025-09-25 21:36:45 +0530

    fssf

--- client/src/pages/dashboard/EventsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const EventsPage = () => {
-    return <h1>Events & News</h1>;
-};
-
-export default EventsPage;
--- client/src/pages/dashboard/NotificationsPage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotificationsPage = () => {
-    return <h1>Notifications</h1>;
-};
-
-export default NotificationsPage;
