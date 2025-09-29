6be48e04c875969c6179b92cf2f96028bfed807c
pickmycourse <tech@pickmycourse.io>
2025-09-29 17:33:43 +0530

    wrrw

--- admin-panel/src/services/classService.js
+++
@@ -1,9 +0,0 @@
-import api from '../utils/api';
-
-const classService = {
-  getClassesByInstitute: (instituteId) => api.get(`/classes/institute/${instituteId}`),
-  createClass: (classData) => api.post('/classes', classData),
-  // Add update and delete functions here
-};
-
-export default classService;
--- admin-panel/src/services/instituteUserService.js
+++
@@ -1,9 +0,0 @@
-import api from '../utils/api';
-
-const instituteUserService = {
-  getUsersByInstitute: (instituteId) => api.get(`/institute-users/institute/${instituteId}`),
-  createInstituteUser: (userData) => api.post('/institute-users', userData),
-  // Add update and delete functions here
-};
-
-export default instituteUserService;
