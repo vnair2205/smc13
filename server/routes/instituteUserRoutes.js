2f2b0afd26db43b05ca1e9ba431061cfe579e570
pickmycourse <tech@pickmycourse.io>
2025-09-29 17:35:19 +0530

    qre

--- server/routes/instituteUserRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const instituteUserController = require('../controllers/instituteUserController');
-const { adminAuth } = require('../middleware/adminAuth');
-
-router.post('/', adminAuth, instituteUserController.createInstituteUser);
-// Add GET, PUT, and DELETE routes
-
-module.exports = router;
