dd0190e7da0809629d3b42034cd5d4b464f7c7f2
pickmycourse <tech@pickmycourse.io>
2025-10-03 20:18:15 +0530

    ffsd

--- admin-panel/src/services/teacherService.js
+++
@@ -1,9 +0,0 @@
-import api from '../utils/api';
-
-const teacherService = {
-  getTeachersByInstitute: (instituteId) => api.get(`/teachers/institute/${instituteId}`),
-  createTeacher: (teacherData) => api.post('/teachers', teacherData),
-  // Add update and delete functions here
-};
-
-export default teacherService;
